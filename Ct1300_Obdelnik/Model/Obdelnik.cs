f32236c14d37c0f94dd44c1f297db287085941df
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-29 14:05:36 +0100

    Vypocty pomoci Getter

--- Ct1300_Obdelnik/Model/Obdelnik.cs
+++ Ct1300_Obdelnik/Model/Obdelnik.cs
@@ -2,8 +2,32 @@
 {
     public class Obdelnik
     {
-        public int StranaA { get; set; }
-        public int StranaB { get; set; }
+        private int stranaA;
+        private int stranaB;
+
+        public int StranaA
+        {
+            get => stranaA; 
+            set
+            {
+                if (stranaA != value)
+                {
+                    if(value < 0)
+                        stranaA = Math.Abs(value) ;
+                    else
+                        stranaA = value;
+                }
+            }
+        }
+        public int StranaB
+        {
+            get => stranaB; 
+            set
+            {
+                if (stranaB != value)
+                    stranaB = Math.Abs(value);
+            }
+        }
         public int Obvod { get; set; }
         public int Obsah { get; set; }
         public double Uhlopricka { get; set; }
---
+++ Ct1300_Obdelnik/Model/Obdelnik2.cs
@@ -0,0 +1,36 @@
+﻿namespace Ct1300_Obdelnik.Model
+{
+    public class Obdelnik2
+    {
+        private int stranaA;
+        private int stranaB;
+
+        public int StranaA
+        {
+            get => stranaA;
+            set
+            {
+                if (stranaA != value)
+                {
+                    if (value < 0)
+                        stranaA = Math.Abs(value);
+                    else
+                        stranaA = value;
+                }
+            }
+        }
+        public int StranaB
+        {
+            get => stranaB;
+            set
+            {
+                if (stranaB != value)
+                    stranaB = Math.Abs(value);
+            }
+        }
+        public int Obvod => 2 * (StranaA + StranaB);
+        public int Obsah => StranaA * StranaB;
+        public double Uhlopricka => Math.Sqrt(Math.Pow(StranaA, 2) + StranaB * StranaB);
+
+    }
+}
